6513f6acfffec09a2943f8adf284789bd88ee24b
Kamil Baćkowski <kbackowski@gmail.com>
2019-04-10 12:38:05 +0200

    Disable request forgery

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module RealtimeChat
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.action_cable.disable_request_forgery_protection = true
+
   end
 end
