e09fbd0cef73d452a7353f8a430641c545dea37b
Kamil Baćkowski <kbackowski@gmail.com>
2019-05-16 15:16:36 +0200

    Set cookie to subdomains

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,1 @@
+Rails.application.config.session_store :cookie_store, key: '_realtimechat', domain: :all, tld_length: 2
--- config/initializers/warden_hooks.rb
+++ config/initializers/warden_hooks.rb
@@ -1,6 +1,6 @@
 Warden::Manager.after_set_user do |user,auth,opts|
   scope = opts[:scope]
-  auth.cookies.signed["#{scope}.id"] = user.id
+  auth.cookies.signed["#{scope}.id"] = { value: user.id, domain: :all, tld_length: 2 } 
 end
 
 Warden::Manager.before_logout do |user, auth, opts|
