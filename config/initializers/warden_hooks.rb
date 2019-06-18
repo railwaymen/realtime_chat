8b8887a7f915e1594fdf33feb5d4e3c7077694da
Kamil Baćkowski <kbackowski@gmail.com>
2019-06-18 09:31:15 +0200

    Use domain cookie only for staging

--- config/initializers/warden_hooks.rb
+++ config/initializers/warden_hooks.rb
@@ -1,6 +1,10 @@
 Warden::Manager.after_set_user do |user,auth,opts|
   scope = opts[:scope]
-  auth.cookies.signed["#{scope}.id"] = { value: user.id } 
+  if Rails.env.staging?
+    auth.cookies.signed["#{scope}.id"] = { value: user.id, domain: :all, tld_length: 2 }
+  else
+    auth.cookies.signed["#{scope}.id"] = { value: user.id }
+  end
 end
 
 Warden::Manager.before_logout do |user, auth, opts|
