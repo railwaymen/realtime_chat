494a3a71043caa15c23349954cc3fc409a162bba
Szymon Chodzidlo <szymon.chodzidlo@railwaymen.org>
2019-06-24 12:39:03 +0200

    Fix code review issues

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,7 +9,7 @@ class RoomsController < BaseController
 
   def show
     @room = Room.kept.find(params[:id])
-    @messages = RoomMessage.includes(:user).where(room: @room).order(id: :asc)
+    @messages = @room.messages.includes(:user).order(id: :asc)
 
     @room_message = RoomMessage.new(room: @room)
   end
