a73739f7d794c5d9ed4e573a563c2daf4b0193b3
Szymon Chodzidlo <szymon.chodzidlo@railwaymen.org>
2019-07-04 13:02:50 +0200

    Add markdown renderer for message body

---
+++ app/javascript/utils/markdown_renderer.js
@@ -0,0 +1,10 @@
+import MarkdownIt from 'markdown-it';
+import emoji from 'markdown-it-emoji';
+
+const markdownRenderer = new MarkdownIt({
+  linkify: true
+});
+
+markdownRenderer.use(emoji);
+
+export default markdownRenderer;
